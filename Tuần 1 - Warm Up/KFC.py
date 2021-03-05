4967bb9be08351b8a8cdc6dd68217a5b04e82ed0
Trương Văn Tuấn <truongtuan2508@gmail.com>
2021-03-05 19:23:05 +0700

    Update KFC.py

--- Tuần 1 - Warm Up/KFC.py
+++ Tuần 1 - Warm Up/KFC.py
@@ -1,6 +1,6 @@
-k, t = map(int, input().split())
-
-if (t // k) % 2 == 0:
-    print(t % k)
-else:
-    print(k - t % k)
+from decimal import *
+F = float(input())
+C = (F - 32.0)/1.8
+K = C + 273.15
+getcontext().prec = 6
+print(Decimal(C).normalize(), Decimal(K).normalize())
